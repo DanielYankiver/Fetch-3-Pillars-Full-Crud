832a91660c6e14ef95019b589764caf3b8073d96
Daniel <dyankiver@gmail.com>
2021-01-20 07:36:45 -0500

    Full Crud Done

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ let turnFoodObjToLi = (food) => {
     // 2) Fill the contents of that element
     foodLi.innerHTML = `
         <p class="js-food middle aligned content">${food.name} - <span>${food.count}</span></p>
-        <button>Increment</button>
+        <button class="increase">Increment</button>
         <button class="delete-button">X</button>
     `
     // 3) Slap on the DOM 
@@ -67,6 +67,10 @@ let turnFoodObjToLi = (food) => {
     let theDeleteButton = foodLi.querySelector("button.delete-button")
                        // outerMostElement.querySelector()
     // console.log(theDeleteButton)
+    let increaseButton = foodLi.querySelector(".increase")
+    // console.log(increaseButton)
+    let countSpan = foodLi.querySelector("span")
+
 
     // 5) Add the event listeners on the appropriate elementss
     theDeleteButton.addEventListener("click", (evt) => {
@@ -79,5 +83,38 @@ let turnFoodObjToLi = (food) => {
                 foodLi.remove()
             })
     })
-}
 
+
+
+
+    increaseButton.addEventListener("click", (evt) => {
+        // console.log("INCREASING", food)
+        let theNewNumber = food.count + 1 
+
+        // UPDATING THE BACKEND 
+        fetch(`http://localhost:3000/foods/${food.id}`, {
+            method: "PATCH",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                count: theNewNumber
+            })
+        })
+            .then(res => res.json())
+            .then((updatedFoodObj) => {
+                // console.log(updatedObj)
+
+                // UPDATING OBJECT IN MEMORY 
+                food.count = updatedFoodObj.count
+                // ^ the line that allows the increment to keep going up w/o refresh
+                
+                // UPDATING THE DOM 
+                countSpan.innerText = updatedFoodObj.count
+            })
+
+    })
+
+
+
+}
